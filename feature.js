f7d7313c4ef651c34407713bb918579708c3bb8c
Baris Senyerli <barissenyerli@gmail.com>
2022-06-02 01:07:12 +0100

    fix for multiple product handles by id

--- feature.js
+++ feature.js
@@ -1,79 +1,216 @@
-document.querySelectorAll("select#pa_malzeme-turu")[0].setAttribute( "onchange", "setData(this.value)" );
-document.querySelectorAll("select#pa_olculer")[0].setAttribute( "onchange", "setData(this.value)" );
+var dataID = document.querySelectorAll("a[data-title=\"Add to wishlist\"")[0].getAttribute("data-original-product-id");
 
-
-window.onload = function() {
-    for (var i = 0; i < document.querySelectorAll("input.wc-pao-addon-checkbox").length ; i++){
-      document.querySelectorAll("input.wc-pao-addon-checkbox")[i].setAttribute( "onchange", "singleChoice(this.value)" );
-    }
-    document.querySelectorAll("div.wc-pao-addon-sac-uzerine-uygulama")[0].hidden = true;
-    document.querySelectorAll("div.wc-pao-addon-pvc-uzerine-uygulama")[0].hidden = true;
-    document.querySelectorAll("div.wc-pao-addon-etiket")[0].hidden = true;
-};
-
-function setData(value){
-    e = document.querySelectorAll("select#pa_malzeme-turu")[0];
-    document.querySelectorAll("input[value=\'25x35\']")[0].checked = false;
-    document.querySelectorAll("input[value=\'35x50\']")[0].checked = false;
-    document.querySelectorAll("input[value=\'50x70\']")[0].checked = false;
-    document.querySelectorAll("input[value=\'70x100\']")[0].checked = false;
-    var selectedOption = e.options[e.selectedIndex].value
-    if (selectedOption == "fotolumen" | selectedOption == "reflektorlu" ) {
-        
-        document.querySelectorAll("div.wc-pao-addon-pvc-uzerine-uygulama")[0].hidden = false
-        document.querySelectorAll("div.wc-pao-addon-etiket")[0].hidden = false
-
-        if (value=="8x12") { document.querySelectorAll("div.wc-pao-addon-sac-uzerine-uygulama")[0].hidden = true; return}
-        if (value=="12x17") { document.querySelectorAll("div.wc-pao-addon-sac-uzerine-uygulama")[0].hidden = true; return }
-        if (value=="17x25") { document.querySelectorAll("div.wc-pao-addon-sac-uzerine-uygulama")[0].hidden = true; return }
-        document.querySelectorAll("div.wc-pao-addon-sac-uzerine-uygulama")[0].hidden = false
-        var classes = {
-            "25x35": "p.wc-pao-addon-302-sac-uzerine-uygulama-2-0",
-            "35x50": "p.wc-pao-addon-302-sac-uzerine-uygulama-2-1",
-            "50x70": "p.wc-pao-addon-302-sac-uzerine-uygulama-2-2",
-            "70x100": "p.wc-pao-addon-302-sac-uzerine-uygulama-2-3",
-        }
-
-
-        var elements = [ 
-            "25x35",
-            "35x50",
-            "50x70",
-            "70x100"
-        ]
-
-        if (value==selectedOption){
-            document.querySelectorAll(classes[elements[0]])[0].hidden = false
-            document.querySelectorAll(classes[elements[1]])[0].hidden = true
-            document.querySelectorAll(classes[elements[2]])[0].hidden = true
-            document.querySelectorAll(classes[elements[3]])[0].hidden = true
-        }
-
-        elements = elements.remove(value)
-        value!=selectedOption ? document.querySelectorAll(classes[value])[0].hidden = false : {};
-        value!=selectedOption ? document.querySelectorAll(classes[elements[0]])[0].hidden = true : {};
-        value!=selectedOption ? document.querySelectorAll(classes[elements[1]])[0].hidden = true : {};
-        value!=selectedOption ? document.querySelectorAll(classes[elements[2]])[0].hidden = true : {};
-
-
-
-    }else{
-        document.querySelectorAll("div.wc-pao-addon-sac-uzerine-uygulama")[0].hidden = true
-        document.querySelectorAll("div.wc-pao-addon-pvc-uzerine-uygulama")[0].hidden = true
-        document.querySelectorAll("div.wc-pao-addon-etiket")[0].hidden = true
-    }
+function getSelectedValue(id) {
+  e = document.querySelectorAll("select#" + id)[0];
+  return e.options[e.selectedIndex].value;
 }
 
-function singleChoice(except) {  // normal checkbox -> radio checkbox
-  inputs = document.querySelectorAll("input.wc-pao-addon-checkbox")
-  for (var i = 0; i < inputs.length ; i++){
-    if (inputs[i].value == except) { continue; }
-    inputs[i].checked = false
+function unhideOnlyOneChoice(selectedKey, elements) {
+  for (var key in elements) {
+    var value = elements[key];
+    if (selectedKey == key)
+      document.querySelectorAll(elements[selectedKey])[0].hidden = false;
+    else document.querySelectorAll(elements[key])[0].hidden = true;
+  }
+  return document.querySelectorAll(dict[selectedKey])[0].hidden === false;
+}
+
+function onlyOneSizeChoicesCanShow() {
+  var currentSize = getSelectedValue("pa_olculer");
+
+  var inputClasses = {
+    "8x12": "p.wc-pao-addon-"+dataID+"-laminasyon-koruyucu-yuzey-kaplama-6-0",
+    "12x17": "p.wc-pao-addon-"+dataID+"-laminasyon-koruyucu-yuzey-kaplama-6-1",
+    "17x25": "p.wc-pao-addon-"+dataID+"-laminasyon-koruyucu-yuzey-kaplama-6-2",
+    "25x35": "p.wc-pao-addon-"+dataID+"-laminasyon-koruyucu-yuzey-kaplama-6-3",
+    "35x50": "p.wc-pao-addon-"+dataID+"-laminasyon-koruyucu-yuzey-kaplama-6-4",
+    "50x70": "p.wc-pao-addon-"+dataID+"-laminasyon-koruyucu-yuzey-kaplama-6-5",
+    "70x100": "p.wc-pao-addon-"+dataID+"-laminasyon-koruyucu-yuzey-kaplama-6-6",
+  };
+
+  unhideOnlyOneChoice(currentSize, inputClasses);
+}
+
+function onlyTwoTypeOfProductsCanNotBeLamination() {
+  var selectedOption = getSelectedValue("pa_malzeme-turu");
+  console.log(selectedOption);
+  var onlyThreeTypeOfProducts = ["sac", "etiket"];
+
+  if (onlyThreeTypeOfProducts.includes(selectedOption)) {
+    // wc-pao-addon wc-pao-addon-laminasyon-koruyucu-yuzey-kaplama
+    // hide this class
+    console.log("hide");
+    document.querySelectorAll(
+      "div.wc-pao-addon-laminasyon-koruyucu-yuzey-kaplama"
+    )[0].hidden = true;
+  } else {
+    console.log("not hide");
+    document.querySelectorAll(
+      "div.wc-pao-addon-laminasyon-koruyucu-yuzey-kaplama"
+    )[0].hidden = false;
   }
 }
 
-Array.prototype.remove = function(from, to){
-  orj = from
-  this.splice(from, (to=[0,from||1,++to-from][arguments.length])<0?this.length+to:to);
-  return this.filter(orj => to.indexOf(orj) === -1);
+// feature.js start
+
+document
+  .querySelectorAll("select#pa_malzeme-turu")[0]
+  .addEventListener("click", () => {
+    onlyTwoTypeOfProductsCanNotBeLamination();
+    onlyOneSizeChoicesCanShow();
+  });
+document
+  .querySelectorAll("select#pa_olculer")[0]
+  .addEventListener("click", () => {
+    onlyTwoTypeOfProductsCanNotBeLamination();
+    onlyOneSizeChoicesCanShow();
+  });
+
+document
+  .querySelectorAll("select#pa_malzeme-turu")[0]
+  .setAttribute("onchange", "setData(this.value)");
+document
+  .querySelectorAll("select#pa_olculer")[0]
+  .setAttribute("onchange", "setData(this.value)");
+
+window.onload = function () {
+  onlyOneSizeChoicesCanShow();
+
+  for (
+    var i = 0;
+    i < document.querySelectorAll("input.wc-pao-addon-checkbox").length;
+    i++
+  ) {
+    document
+      .querySelectorAll("input.wc-pao-addon-checkbox")
+      [i].setAttribute("onchange", "singleChoice(this.value)");
+  }
+  document.querySelectorAll(
+    "div.wc-pao-addon-sac-uzerine-uygulama"
+  )[0].hidden = true;
+  document.querySelectorAll(
+    "div.wc-pao-addon-pvc-uzerine-uygulama"
+  )[0].hidden = true;
+  document.querySelectorAll("div.wc-pao-addon-etiket")[0].hidden = true;
 };
+
+function setData(value) {
+  e = document.querySelectorAll("select#pa_malzeme-turu")[0];
+  document.querySelectorAll("input[value=\'25x35\']")[0].checked = false;
+  document.querySelectorAll("input[value=\'35x50\']")[0].checked = false;
+  document.querySelectorAll("input[value=\'50x70\']")[0].checked = false;
+  document.querySelectorAll("input[value=\'70x100\']")[0].checked = false;
+  var selectedOption = e.options[e.selectedIndex].value;
+
+  // case 1
+  if ((selectedOption == "fotolumen") | (selectedOption == "reflektorlu")) {
+    document.querySelectorAll(
+      "div.wc-pao-addon-pvc-uzerine-uygulama"
+    )[0].hidden = false;
+    document.querySelectorAll("div.wc-pao-addon-etiket")[0].hidden = false;
+
+    if (value == "8x12") {
+      document.querySelectorAll(
+        "div.wc-pao-addon-sac-uzerine-uygulama"
+      )[0].hidden = true;
+      return;
+    }
+    if (value == "12x17") {
+      document.querySelectorAll(
+        "div.wc-pao-addon-sac-uzerine-uygulama"
+      )[0].hidden = true;
+      return;
+    }
+    if (value == "17x25") {
+      document.querySelectorAll(
+        "div.wc-pao-addon-sac-uzerine-uygulama"
+      )[0].hidden = true;
+      return;
+    }
+    document.querySelectorAll(
+      "div.wc-pao-addon-sac-uzerine-uygulama"
+    )[0].hidden = false;
+    var classes = {
+      "25x35": "p.wc-pao-addon-"+dataID+"-sac-uzerine-uygulama-3-0",
+      "35x50": "p.wc-pao-addon-"+dataID+"-sac-uzerine-uygulama-3-1",
+      "50x70": "p.wc-pao-addon-"+dataID+"-sac-uzerine-uygulama-3-2",
+      "70x100": "p.wc-pao-addon-"+dataID+"-sac-uzerine-uygulama-3-3",
+    };
+
+    var elements = ["25x35", "35x50", "50x70", "70x100"];
+
+    if (value == selectedOption) {
+      document.querySelectorAll(classes[elements[0]])[0].hidden = false;
+      document.querySelectorAll(classes[elements[1]])[0].hidden = true;
+      document.querySelectorAll(classes[elements[2]])[0].hidden = true;
+      document.querySelectorAll(classes[elements[3]])[0].hidden = true;
+    }
+
+    elements = elements.remove(value);
+    value != selectedOption
+      ? (document.querySelectorAll(classes[value])[0].hidden = false)
+      : {};
+    value != selectedOption
+      ? (document.querySelectorAll(classes[elements[0]])[0].hidden = true)
+      : {};
+    value != selectedOption
+      ? (document.querySelectorAll(classes[elements[1]])[0].hidden = true)
+      : {};
+    value != selectedOption
+      ? (document.querySelectorAll(classes[elements[2]])[0].hidden = true)
+      : {};
+  } else {
+    document.querySelectorAll(
+      "div.wc-pao-addon-sac-uzerine-uygulama"
+    )[0].hidden = true;
+    document.querySelectorAll(
+      "div.wc-pao-addon-pvc-uzerine-uygulama"
+    )[0].hidden = true;
+    document.querySelectorAll("div.wc-pao-addon-etiket")[0].hidden = true;
+  }
+}
+
+function singleChoice(except) {
+  inputs = document.querySelectorAll("input.wc-pao-addon-checkbox");
+  for (var i = 0; i < inputs.length; i++) {
+    if (inputs[i].value == except) {
+      continue;
+    }
+    inputs[i].checked = false;
+  }
+}
+
+Array.prototype.remove = function (from, to) {
+  orj = from;
+  this.splice(
+    from,
+    (to = [0, from || 1, ++to - from][arguments.length]) < 0
+      ? this.length + to
+      : to
+  );
+  return this.filter((orj) => to.indexOf(orj) === -1);
+};
+
+// feature.js end
+
+function specifications() {
+  var openHideDiv = document.getElementsByClassName(
+    "wc-pao-addons-container"
+  )[0].style;
+  if (openHideDiv.display == "block") {
+    openHideDiv.display = "none";
+  } else {
+    openHideDiv.display = "block";
+  }
+  change("up-down-icon");
+  onlyTwoTypeOfProductsCanNotBeLamination();
+  onlyOneSizeChoicesCanShow();
+}
+
+function change(iconID) {
+  if (document.getElementById(iconID).className == "icon-angle-up") {
+    document.getElementById(iconID).className = "icon-angle-down";
+  } else {
+    document.getElementById(iconID).className = "icon-angle-up";
+  }
+}
